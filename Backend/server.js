53c72c3ce6eeb501dfa28bb988a9d4a94a8da3d7
Abhishek <sabaneabhishek0110@gmail.com>
2025-05-24 14:16:40 +0530

    CORS changed

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ['http://localhost:5173', "https://researc-hub-pbl.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
